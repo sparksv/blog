9cd5c76e174863301ec481dd78f4554af983ac95
Valerie Sparks <valeriesparks@Valeries-MacBook-Pro.local>
2014-02-05 20:19:07 -0500

    I love cCodin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Blog::Application.routes.draw do
-  get "welcome/indexcd"
-  resources :posts
 
+  root "welcome#index"
+  
   get "welcome/index"
+  get "welcome" => "welcome#index"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
