b68a508f8738dd14de22c654d646f612ef9e92f0
Valerie Sparks <valeriesparks@Valeries-MacBook-Pro.local>
2014-02-17 18:17:47 -0500

    handshake

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,29 @@
 class WelcomeController < ApplicationController
 
   def index
+  	@our_names = ['one','two']
+  	@first_name = session[:user_first_name]
+  	@last_name = session[:user_last_name]
+
   end
+
   def visitor
   end
 
+  
 
+  puts "params: #{params}"
+
+first_name = params[:firstname]
+last_name = params[:lastname]
+
+session[:user_first_name] = first_name
+session[:user_last_name] = last_name
+
+puts "FIRSTNAME: #{first_name}"
+puts "LASTNAME: #{last_name}"
+
+  puts "FIRSTNAME: #{params[:firstname]}"
 end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Blog::Application.routes.draw do
   get "welcome/index"
   get "welcome" => "welcome#index"
   get "welcome/visitor" => "welcome#visitor"
+  get "welcome/bio" => "welcome#bio"
 
+  post "welcome/index" => "welcome#handshake"
+  post welcome" +> "welcomeindex"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
